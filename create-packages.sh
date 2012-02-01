b881c8f25791c5704bb2f353d070c3d37e0c3a1d
Marko Saukko <marko.saukko@cybercom.com>
2012-02-01 13:56:54 +0200

    Large refactoring. Now these files are used with https://gitorious.org/meego-developer-edition-for-n900/repomd-pattern-builder
    Signed-off-by: Marko Saukko <marko.saukko@cybercom.com>

---
+++ create-packages.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+rm -rf packages/
+mkdir -p packages/
+
+pushd patterns &> /dev/null
+
+PATTERN_DIRS=`ls -1 -d */`
+
+for PATTERN_DIR in ${PATTERN_DIRS}
+do
+    if [ -f $PATTERN_DIR/VERSION ]
+    then
+        # VERSION file is present if there is multiple .yaml files in dir.
+        VERSION=`cat $PATTERN_DIR/VERSION`
+    else
+        # Check version from .yaml if there is only one .yaml file.
+        VERSION=`awk '/Version: (\d)*/{print $2;exit}' ${PATTERN_DIR}/*.yaml`
+    fi
+    cp Makefile ${PATTERN_DIR}/
+    PATTERN_NAME=`echo ${PATTERN_DIR} | sed "s!/!!g"`
+    tar --exclude=*~ -cJf ../packages/pattern-${PATTERN_NAME}-${VERSION}.tar.xz ${PATTERN_DIR}/
+    rm ${PATTERN_DIR}/Makefile
+done
+popd &> /dev/null
